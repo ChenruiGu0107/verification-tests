70ece0d3121679aab393276783be260cc17ff31f
Aleksandar Kostadinov <akostadi@redhat.com>
2015-08-19 15:55:23 +0300

    some tidy-up of PR 2334

--- features/step_definitions/policy.rb
+++ features/step_definitions/policy.rb
@@ -1,11 +1,21 @@
 When /^I give (.+?) role to (the [a-z]+) user$/ do |role_name, which_user|
   user_name=env.users[word_to_num(which_user)].name
-  user.cli_exec(:add_role_to_user, role:role_name,user_name:user_name)
+  user.cli_exec(
+    :add_role_to_user,
+    role: role_name,
+    user_name: user_name,
+    n: project.name
+  )
 end
 
 When /^I remove (.+?) role from (the [a-z]+) user$/ do |role_name, which_user|
   user_name=env.users[word_to_num(which_user)].name
-  user.cli_exec(:remove_role_from_user, role:role_name, user_name:user_name)
+  user.cli_exec(
+    :remove_role_from_user,
+    role: role_name,
+    user_name: user_name,
+    n: project.name
+  )
 end
 
 Given /^(the [a-z]+) user is cluster-admin$/ do |which_user|
--- features/test/policy.feature
+++ features/test/policy.feature
@@ -1,6 +1,6 @@
 Feature: Add admin user to current project
   Scenario: Add admin user to current project
-    When I create a new project 
+    When I create a new project
     Then the step should succeed
     When I give admin role to the second user
     Then the step should succeed
