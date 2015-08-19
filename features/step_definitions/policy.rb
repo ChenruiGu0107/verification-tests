84cc9c200122348b604e9fab711599d5c7386818
anpingli <anli@redhat.com>
2015-08-19 14:17:49 +0800

    broker policy to subcommand, and handle failure in Given step

--- features/step_definitions/policy.rb
+++ features/step_definitions/policy.rb
@@ -1,7 +1,6 @@
 When /^I give (.+?) role to (the [a-z]+) user$/ do |role_name, which_user|
    user_name=env.users[word_to_num(which_user)].name
    user.cli_exec(:add_role_to_user, role:role_name,user_name:user_name)
-   
 end
 
 When /^I remove (.+?) role from (the [a-z]+) user$/ do |role_name, which_user|
@@ -12,21 +11,21 @@ end
 Given /^(the [a-z]+) user is cluster-admin$/ do |which_user|
    user_name=env.users[word_to_num(which_user)].name
     _admin = admin
-
-    _admin.cli_exec(
-       :oadm_policy,
-       policy_type: "add-cluster-role-to-user",
+    @result=_admin.cli_exec(
+       :oadm_add_cluster_role_to_user,
        role_name: "cluster-admin",
        user_name: user_name
     )
-
-    teardown_add {
-      res = _admin.cli_exec(
-        :oadm_policy,
-        policy_type: "remove-cluster-role-from-user",
-        role_name: "cluster-admin",
-        user_name: user_name
-      )
-      raise "could not restore user #{user_name}" unless res[:success]
-    }
+    if @result[:success]
+      teardown_add {
+        @res = _admin.cli_exec(
+          :oadm_remove_cluster_role_from_user,
+          role_name: "cluster-admin",
+          user_name: user_name
+        )
+        raise "could not restore user #{user_name}" unless @res[:success]
+      }
+    else
+        raise "could not give #{user_name} role cluster-admin"
+    end
 end
