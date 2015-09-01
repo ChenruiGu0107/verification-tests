b2beaaf49d3410c8fc5899a76451ddc6e4647099
Chris Ryan <cryan@redhat.com>
2015-08-31 19:29:17 -0700

    Updated 'expose' step to be a 'When' step, fixed attendant feature files.

--- features/step_definitions/route.rb
+++ features/step_definitions/route.rb
@@ -2,7 +2,7 @@ require 'openshift/route'
 require 'openshift/service'
 
 # e.g I expose the "myapp" service
-Given /^I expose(?: the)?(?: "(.+?)")? service$/ do |service_name|
+When /^(?:the)?(?: "(.+?)")? service is exposed$/ do |service_name|
   r = route(service_name, service(service_name))
   @result = r.create(by: user)
 end
