e2d80c737590baa04ce0b8540f072e9f7b03283c
Peter Ruan <pruan@redhat.com>
2016-01-20 10:30:55 -0800

    fix step to not use class variable

--- features/step_definitions/admin.rb
+++ features/step_definitions/admin.rb
@@ -1,15 +1,13 @@
 # admin related steps
 
 Given /^I have (?:(at least ))?(\d+) nodes?$/ do |quantifier, nodes|
-  #ensure_admin_tagged
   num_of_nodes = nodes.to_i
-  @nodes = env.nodes
-  if quantifier
-    res = @nodes.count >= num_of_nodes
-  else
-    res = @nodes.count == num_of_nodes
-  end
-  raise "number of nodes '#{@nodes.count}' in the setup does not meet the criteria of #{quantifier} #{nodes} nodes"
+  nodes_found = env.nodes.count
   @result = {}
-  @result[:success] = res
+  if quantifier
+    @result[:success] = nodes_found >= num_of_nodes
+  else
+    @result[:success] = nodes_found == num_of_nodes
+  end
+  raise "number of nodes '#{nodes_found}' in the setup does not meet the criteria of #{quantifier}#{nodes} nodes" unless @result[:success]
 end
