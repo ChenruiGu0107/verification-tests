f7bd7ceb9d45bf797d0b0c6d9746bbc3ce983a94
Peter Ruan <pruan@redhat.com>
2016-05-06 14:39:41 -0700

    removed run away comment

--- features/step_definitions/node.rb
+++ features/step_definitions/node.rb
@@ -5,7 +5,7 @@ Given /^I select a random node's host$/ do
   @host = env.node_hosts.sample
 end
 
-# @host from World will be used.  If
+# @host from World will be used.
 Given /^I run commands on the host:$/ do |table|
   raise "You must set a host prior to running this step" if host.nil?
   table.raw.flatten.map do |cmd|
