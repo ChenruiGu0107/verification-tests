a6b8a3c3891a0a31f36335a3705137c86b155a0a
Aleksandar Kostadinov <akostadi@redhat.com>
2016-08-15 11:52:59 +0300

    set more meningful default clipboard

--- features/step_definitions/env.rb
+++ features/step_definitions/env.rb
@@ -13,7 +13,7 @@ end
 Given /^I store default router IPs in the#{OPT_SYM} clipboard$/ do |cb_name|
   raise "get router info as regular user" if CucuShift::ClusterAdmin === user
 
-  cb_name = 'tmp' unless cb_name
+  cb_name = 'ips' unless cb_name
   cb[cb_name] = env.router_ips(user: user, project: project(generate: false))
   logger.info cb[cb_name]
 end
