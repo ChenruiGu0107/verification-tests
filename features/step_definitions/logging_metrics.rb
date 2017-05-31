ChenruiGu0107/verification-tests

commit b33598e7d9579b1124cfecf27ad56297ddec5e28

added config to allow user to control ansible output log level (#5072)

features/step_definitions/logging_metrics.rb
@@ -252,17 +252,13 @@ Given /^(logging|metrics) service is (installed|uninstalled) (?:in|from) the#{OP
   begin
     step %Q/I switch to cluster admin pseudo user/
     step %Q/I have a pod with openshift-ansible playbook installed/
-
     if svc_type == 'logging'
       ansible_template_path = "/usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-logging.yml"
     else
       ansible_template_path = "/usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-metrics.yml"
     end
-
     step %Q/I execute on the pod:/, table(%{
-      | bash                                                                                                  |
-      | -c                                                                                                    |
-      | cd /usr/share && ansible-playbook -i /tmp/<%= "#{new_path}" %> -vvv <%= "#{ansible_template_path}" %> |
+      | ansible-playbook | -i | /tmp/=#{new_path} | #{conf[:ansible_log_level]} | #{ansible_template_path} |
       })
     step %Q/the step should succeed/
 

config/config.yaml
@@ -11,7 +11,15 @@ global:
   # default to git, specify branch by adding '#<branch_name>' after the keyword git
   # if no branch is specified, we will use the commit_id we get from qe-inventory-file from master node
   openshift_ansible_installer: git
-
+  # use this parameter to control the ansible output verboseness
+  # valid parameters are
+  # -v        The output data is displayed.
+  # -vv      Both the output and input data are displayed.
+  # -vvv    Includes information about connections to managed hosts.
+  # -vvvv  Adds extra verbosity options to the connection plug-ins, including
+  #          the users being used in the managed hosts to execute scripts, and
+  #          what scripts have been executed.
+  ansible_log_level: -vv
   # base docker registry prefixes for quickstarts;
   #   (one is for the upstream community supported *projects* and the other one
   #   is for the Red Hat enterprise supported *products*);

commit 330a970828137709796e118ecaefda94f821fc85

hot fix for a typo introduced by PR #5072

features/step_definitions/logging_metrics.rb
@@ -258,7 +258,7 @@ Given /^(logging|metrics) service is (installed|uninstalled) (?:in|from) the#{OP
       ansible_template_path = "/usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-metrics.yml"
     end
     step %Q/I execute on the pod:/, table(%{
-      | ansible-playbook | -i | /tmp/=#{new_path} | #{conf[:ansible_log_level]} | #{ansible_template_path} |
+      | ansible-playbook | -i | /tmp/#{new_path} | #{conf[:ansible_log_level]} | #{ansible_template_path} |
       })
     step %Q/the step should succeed/
 