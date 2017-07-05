a90561384e0869c6bf2bb753e51496fe44a47360
Aleksandar Kostadinov <akostadi@redhat.com>
2017-07-05 23:51:09 +0300

    restoring a missing file means to remove it

--- features/step_definitions/node.rb
+++ features/step_definitions/node.rb
@@ -98,7 +98,7 @@ Given /^I use the #{QUOTED} node$/ do | host |
   @host = node(host).host
 end
 
-# restore particular file after scenario
+# restore particular file after scenario; if missing, then removes it
 Given /^the #{QUOTED} file is restored on host after scenario$/ do |path|
   _host = @host
 
@@ -109,16 +109,26 @@ Given /^the #{QUOTED} file is restored on host after scenario$/ do |path|
 
   # tar the file on host so we can restore with permissions later
   @result = _host.exec_admin("find '#{path}' -maxdepth 0 -type f")
-  unless @result[:success] && !@result[:response].empty?
-    raise "target path not a file"
+  if @result[:success]
+    if @result[:response].empty?
+      raise "target path not a file"
+    else
+      # file exist
+      @result = _host.exec_admin("tar --selinux --acls --xattrs -cvPf '#{path}.tar' '#{path}'")
+      raise "could not archive target file" unless @result[:success]
+      _restore_command = "tar xvPf '#{path}.tar' && rm -f '#{path}.tar'"
+    end
+  else
+    # file does not exist
+    _restore_command = "rm -f '#{path}'"
   end
 
-  @result = _host.exec_admin("tar --selinux --acls --xattrs -cvPf '#{path}.tar' '#{path}'")
-  raise "could not archive target file" unless @result[:success]
 
   teardown_add {
-    @result = _host.exec_admin("tar xvPf '#{path}.tar' && rm -f '#{path}.tar'")
-    raise "could not restore #{path} on #{_host.hostname}" unless @result[:success]
+    @result = _host.exec_admin(_restore_command)
+    unless @result[:success]
+      raise "could not restore #{path} on #{_host.hostname}"
+    end
   }
 end
 
