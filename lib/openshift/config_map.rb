e6897b5bc5487619b600125a4cf299ede1d6be88
Aleksandar Kostadinov <akostadinov@gmail.com>
2018-07-21 01:23:30 +0300

    LimitRange support (#6558)
    * remove redundant methods from ConfigMap
    
    * LimitRange support

--- lib/openshift/config_map.rb
+++ lib/openshift/config_map.rb
@@ -9,19 +9,6 @@ module CucuShift
       rr.dig('data')
     end
 
-    # # @name: the name to the data hash element we want to be parsed
-    # # @return: a parsed YAML in the form of a Hash
-    # def parsed_xml_data(user: nil, name: nil, cached: true, quiet: false)
-    #   raw_data = self.data(user: user, cached: cached, quiet: quiet)
-    #   parsed_data = YAML.load(raw_data.dig(name))
-    #   return parsed_data
-    # end
-
-    def namespace(user: nil, cached: true, quiet: false)
-      rr = raw_resource(user: user, cached: cached, quiet: quiet)
-      rr.dig('metadata', 'namespace')
-    end
-
     def dig(*keys, user: nil, cached: true, quiet: false)
       if keys.size == 0
         raise "specify keys to dig for within the config map data"
@@ -33,16 +20,6 @@ module CucuShift
       else
         return value.dig(*keys)
       end
-    end
-
-    def labels(user: nil, cached: true, quiet: false)
-      rr = raw_resource(user: user, cached: cached, quiet: quiet)
-      rr.dig('metadata', 'labels')
-    end
-
-    def created(user: nil, cached: true, quiet: false)
-      rr = raw_resource(user: user, cached: cached, quiet: quiet)
-      rr.dig('metadata', 'creationTimestamp')
     end
 
     def value_of(key, user: nil, cached: true, quiet: false)
